a9a6194338033741afbf5f99f5c32e4ad77895c1
majin <majin@beecredit.com>
2022-03-30 17:12:40 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,60 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    result: {
+      data: {
+        list: [
+          "61ea2271e4b00bbaf7cd2e1d",
+          "61ea227be4b0d66d6c3e9e12",
+          "61ea2285e4b0d66d6c3e9e1a",
+          "61ea228ee4b0d66d6c3e9e22",
+          "61ee07cde4b00bbaf7cd2ebb",
+          "61ef9b2fe4b0d66d6c3e9f27",
+          "61ef9b38e4b00bbaf7cd2f2b",
+          "61ef9b41e4b0d66d6c3e9f36",
+          "61ef9b4be4b00bbaf7cd2f3a",
+          "61ef9b55e4b0d66d6c3e9f45",
+          "61ef9bbfe4b00bbaf7cd2f4d",
+          "61ef9bcbe4b00bbaf7cd2f57",
+          "61f0ad68e4b0d66d6c3e9f96",
+          "61f0adf5e4b00bbaf7cd2f8c",
+          "9Q95WE39IAGJ5XCK"
+        ]
+      },
+    },
+    
+      v61ea2271e4b00bbaf7cd2e1d: {
+        list: [{
+            name: "A",
+            create_time: "2022-01-21 11:03:14"
+          },
+          {
+            name: "B",
+            create_time: "2022-01-21 11:03:16"
+          },
+          {
+            name: "C",
+            create_time: "2022-01-21 11:03:19"
+          },
+          {
+            name: "D",
+            create_time: "2022-01-21 11:03:20"
+          },
+          {
+            name: "E",
+            create_time: "2022-01-21 11:03:22"
+          }
+        ]
+       
+    }
+
+  },
+  onLoad() {
+
+ 
+
+  },
+})
